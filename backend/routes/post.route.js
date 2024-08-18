4ccb6af6593aa3847ffb808c6bb1ee9dfa8e9fdd
Jane Doe <jane.doe@example.com>
2024-08-18 23:04:05 +0600

    frist-commit

---
+++ backend/routes/post.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
+
+
+const router = express.Router();
+
+
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,37 @@
-console.log('hello world')
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
+import connectMongoDB from "./db/connectMongoDB.js";
+import { v2 as cloudinary } from "cloudinary";
+import postRoutes from "./routes/post.route.js"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// / Add this middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send("Server is ready")
+});
+
+app.listen(PORT, () => {
+    connectMongoDB()
+    console.log("Server is running on port 8000")
+})
