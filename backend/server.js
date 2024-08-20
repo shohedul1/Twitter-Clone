02e98e8c48ed28bf48d013b3f5ba688d7ffe1205
Jane Doe <jane.doe@example.com>
2024-08-20 14:38:04 +0600

    hellow

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js"
@@ -17,12 +18,17 @@ cloudinary.config({
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend
+    methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
+    credentials: true                // Allow credentials (if needed)
+}));
 const PORT = process.env.PORT || 5000;
 // / Add this middleware to parse JSON bodies
-app.use(express.json({limit:"5mb"})); //to parse req.body
+app.use(express.json({ limit: "5mb" })); //to parse req.body
 // limit shouldn't be too high to prevent DOS
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://twitter-clone-mgjy.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  }
+ 
 })
