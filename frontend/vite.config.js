6ae36228a704cf32f4e6e217e0b8d7db0192a74e
Jane Doe <jane.doe@example.com>
2024-08-20 15:08:29 +0600

    final-comit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://twitter-clone-mgjy.onrender.com",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
